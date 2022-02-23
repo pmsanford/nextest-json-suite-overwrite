d8faa17561d27d4d41aadc95c6ac56189e2ce306
Paul Sanford <me@paulsanford.net>
2022-02-23 14:08:15 -0600

    Repro issue

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_lib() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_bin() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
